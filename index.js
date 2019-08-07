6e71d750ec3aafdcbb518b350d119d6612c79d37
McDublets <53711112+McDublets@users.noreply.github.com>
2019-08-06 21:01:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -105,6 +105,14 @@ bot.on('message', msg=>{
         msg.channel.sendMessage('Mike fucks em, and HES BIG GAY NIGGA');
     }
 })
+bot.on('message', msg=>{
+    if (msg.author == bot.user) {
+        return
+    }
+    if (msg.content.includes("Horses")){
+        msg.channel.sendMessage('Mike fucks em, and HES BIG GAY NIGGA');
+    }
+})
 
 bot.on('message', message=>{
   
